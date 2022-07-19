40bc2806506798d729604ebb2d996aaafc0e8d50
Peter Forsyth <paiforsyth@hotmail.com>
2022-07-18 21:41:44 -0400

    no hedgeg

--- experiments/no_hedge.py
+++ experiments/no_hedge.py
@@ -1,6 +1,11 @@
 from pfhedge.instruments import BrownianStock, EuropeanOption
 def simulate_european():
-    n_paths = 100000
+    n_paths = 10000
     stock = BrownianStock()
     option = EuropeanOption(stock)
-    result = option.simulate(n_paths=n_paths)
+    option.simulate(n_paths=n_paths)
+    print(option.payoff().shape)
+
+
+if __name__ =="__main__":
+    simulate_european()
